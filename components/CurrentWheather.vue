11c4eaf76be55ab1037c136e3b80161c4ea7c111
Lars Jönsson <crymeat@me.com>
2019-01-07 18:09:04 +0100

    now the app works, removed code in div tag

--- components/CurrentWheather.vue
+++ components/CurrentWheather.vue
@@ -2,18 +2,19 @@
   <section class="container">
    <div class="spinner mx-auto" v-if="doneLoading === 'false'"></div>
     <main class="mx-auto text-center font-sans" v-if="doneLoading === 'OK'">
-      <div v-if="wheather.currently.temperature.toFixed(0) >= 0">
+      <div>
         <img class="my-5" :src="vader[wheather.currently.icon]" alt="Ikon på aktuellt väder" :title="wheather.currently.icon">
+        <span></span>
         <h1 class="text-5xl">{{ wheather.currently.temperature.toFixed(0) }}&deg;C</h1>
-        <h2 class="text-3xl mb-8">{{ wheather.currently.summary}}</h2>
+        <h2 class="text-3xl mb-8">{{ wheather.currently.summary }}</h2>
         <p class="mb-2">Känns som {{ wheather.currently.apparentTemperature.toFixed(0)}}&deg;C</p>
-        <p class="mb-2">Vind: {{ wheather.currently.windSpeed.toFixed(0)}} m/s</p>
-        <p class="mb-2">{{ wheather.hourly.summary}}</p>
+        <p class="mb-2">Vind: {{ wheather.currently.windSpeed.toFixed(0) }} m/s</p>
+        <p class="mb-2">{{ wheather.hourly.summary }}</p>
       </div>
         
         <div class="dailyWeather mt-10">
           <h4 class="text-center mb-3">Väderprognos</h4>
-          <div v-if="doneLoading === 'OK'" class="daily text-center mx-auto">
+          <div  class="daily text-center mx-auto">
           <div class="wheaterTomorrow">
             <img :src="vader[wheather.daily.data[1].icon]" class="mb-5 mx-5" alt="">
             <pre>↑{{ wheather.daily.data[1].temperatureMax.toFixed(0) }}&deg;C</pre>
@@ -70,9 +71,7 @@ export default {
       'doneLoading'
     ])
   },
-  methods: {
 
-  }
 }
 </script>
 
