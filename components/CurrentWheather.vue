9ec435eab4976b30b6a6d899bf89c37b2e983402
Lars Jönsson <crymeat@me.com>
2019-01-23 19:58:29 +0100

    more design stuff and options to Geolocation

--- components/CurrentWheather.vue
+++ components/CurrentWheather.vue
@@ -4,31 +4,31 @@
     <main class="mx-auto" v-if="doneLoading === 'OK'">
       <div class="position text-left my-10">
         <img src="~/assets/images/position.svg" alt="GPS position image" style="width: 20px">
-        <span class="my-10 font-bold text-xl">{{position.data.results[0].address_components[1].long_name}}</span>
+        <span class="my-10 font-bold text-xl">{{position.data.results[0].address_components[2].long_name}}</span>
       </div>
       <div class="currently text-left">
         <div class="weatherNow mb-10">
           <h1 class="text-5xl">{{ wheather.currently.temperature.toFixed(0) }}&deg;C</h1>
-          <img :src="vader[wheather.currently.icon]" class="mx-5" alt="">
+          <img :src="vader[wheather.currently.icon]" class="today" alt="Image on current weather">
           <p class="mb-2 font-bold text-sm">Känns som {{ wheather.currently.apparentTemperature.toFixed(0)}}&deg;C</p>
         </div>
         <h2 class="my-3">{{ wheather.currently.summary }}</h2>
         <p class="mb-2"><span class="font-bold">Vind:</span> {{ wheather.currently.windSpeed.toFixed(0) }} m/s</p>
         <p class="mb-10">{{ wheather.hourly.summary }}</p>
         <p class="mb-2">Temperaturer under {{ convertToDay($moment.unix(wheather.currently.time).days() ) }}en</p>
-        <span class="mr-3 font-bold">↑{{ wheather.daily.data[0].temperatureMax.toFixed(0) }}&deg;C</span>
-        <span class="font-bold">↓{{ wheather.daily.data[0].temperatureMin.toFixed(0) }}&deg;C</span>
+        <span class="mr-3 text-red-lighter font-bold">↑{{ wheather.daily.data[0].temperatureMax.toFixed(0) }}&deg;C</span>
+        <span class="font-bold text-blue-lighter">↓{{ wheather.daily.data[0].temperatureMin.toFixed(0) }}&deg;C</span>
       </div>
         
         <div class="dailyWeather mt-10">
           <h4 class="text-center mb-3 underline">7 dagarsprognos</h4>
-          <div class="daily text-center mx-auto my-10">
+          <div class="daily text-left my-10">
           
-          <div v-for="(day, index) in wheather.daily.data.slice(1)" :key="day.index">
-              <img :src="vader[day.icon]" class="mb-5 mx-5 weather-icon" alt="">
-              <p class="font-bold">↑{{ day.temperatureMax.toFixed(0) }}&deg;C</p>
-              <p class="font-bold">↓{{ day.temperatureMin.toFixed(0) }}&deg;C</p>
-              <p class="mt-1"> {{ convertToDay($moment.unix(day.time).days() ) }} </p>
+          <div class="mx-5" v-for="(day, index) in wheather.daily.data.slice(1)" :key="day.index">
+              <p class="my-5 text-light font-bold"> {{ convertToDay($moment.unix(day.time).days() ) }} </p>
+              <img :src="vader[day.icon]" class="mb-5 weather-icon block mx-auto" alt="">
+              <p class="font-bold text-red-lighter">↑{{ day.temperatureMax.toFixed(0) }}&deg;C</p>
+              <p class="font-bold text-blue-lighter">↓{{ day.temperatureMin.toFixed(0) }}&deg;C</p>
               <p class="mt-1"> {{ $moment.unix(day.time).format("DD/MM") }} </p>
               <br>
           </div>
@@ -41,6 +41,7 @@
 <script>
 import {mapState} from 'vuex';
 import moment from 'moment'
+
 export default {
   data() {
     return {
@@ -93,7 +94,7 @@ export default {
   margin: 0 auto
   padding: 0 1rem
   .currently
-    background: hsla(255,255,255, 0.2)
+    background: rgba(255,255,255, 0.2)
     padding: 1rem
     border-radius: 12px
     .weatherNow
@@ -102,18 +103,20 @@ export default {
       align-items: center
       justify-content: center
       margin: auto
-      //grid-template-areas: "main main sidebar sidebar"
-      //grid-area: header
       @media (max-width:330px)
         display: block
         img
           margin: 1rem 0
     img
-      width: 100px
+      width: 100%
 .daily
   display: flex
-  flex-wrap: wrap
-  justify-content: center
+  flex-wrap: nowrap
+  overflow-x: auto
+  border-radius: 12px
+  background: rgba(255,255,255, 0.2)
+  -webkit-overflow-scrolling: touch
+  -ms-overflow-style: -ms-autohiding-scrollbar
   .wheaterTomorrow
     .weather-icon
       max-width: 100%
--- store/index.js
+++ store/index.js
@@ -11,15 +11,17 @@ const createStore = () => {
       error: ''
     },
     actions: {
-      async getWheather({commit}) {
-        
+      async getWheather({
+        commit
+      }) {
+
         const CORS = 'https://cors-anywhere.herokuapp.com/';
         /* const lat = '64.1137179';
         const long = '16.1950004'; */
         const KEY = '0103817e28177f2124d9e291e737ed04';
-    
+
         if (process.browser) {
-          
+
           if (navigator.geolocation) {
 
             // var options = {
@@ -27,31 +29,32 @@ const createStore = () => {
             //   timeout: 5000,
             //   maximumAge: 0
             // };
-            navigator.geolocation.getCurrentPosition( (location, options) => {
-              
+            navigator.geolocation.getCurrentPosition((location, options) => {
+
               let latitude = location.coords.latitude;
               let longitude = location.coords.longitude;
+              let accuracy = location.coords.accuracy;
               console.log(latitude + " " + longitude);
               axios.all([
-                axios.get(`${CORS}https://api.darksky.net/forecast/${KEY}/${latitude},${longitude}?lang=sv&units=auto&exclude="flags,alerts,offset,minutely"`),
-                axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyBLilWwDtdymiAABzXQzt_AZ-_fiBRrrbw`)
-              ])
-              .then(axios.spread((data, pos) => {
-                commit('SET_STATUS', data.statusText)
-                let wheather = data.data;
-                let position = pos;
-                commit('SET_WHEATHER', wheather)
-                commit('SET_POSITION', position)
-                console.log(wheather);
-                console.log(pos);
-                
-              }))
-              .catch((error) => {
-                console.log(error);
-                commit('SET_ERROR', {
-                  state: 'Error'
-                });
-              })
+                  axios.get(`${CORS}https://api.darksky.net/forecast/${KEY}/${latitude},${longitude}?lang=sv&units=auto&exclude="flags,alerts,offset,minutely"`),
+                  axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyBLilWwDtdymiAABzXQzt_AZ-_fiBRrrbw`)
+                ])
+                .then(axios.spread((data, pos) => {
+                  commit('SET_STATUS', data.statusText)
+                  let wheather = data.data;
+                  let position = pos;
+                  commit('SET_WHEATHER', wheather)
+                  commit('SET_POSITION', position)
+                  console.log(wheather);
+                  console.log(pos);
+                  console.log(accuracy);
+                }, {maximumAge: 0, timeout:5000, enableHighAccuracy: true}))
+                .catch((error) => {
+                  console.log(error);
+                  commit('SET_ERROR', {
+                    state: 'Error'
+                  });
+                })
             })
           } else {
             // geolocation is not supported
@@ -59,20 +62,20 @@ const createStore = () => {
             alert('geolocation is not enabled on this browser')
           }
         }
-        
-       
+
+
 
       }
-      
+
     },
     mutations: {
-      SET_WHEATHER (state, wheather) {
+      SET_WHEATHER(state, wheather) {
         state.wheather = wheather;
       },
-      SET_POSITION (state, position) {
+      SET_POSITION(state, position) {
         state.position = position;
       },
-      SET_STATUS (state, doneLoading) {
+      SET_STATUS(state, doneLoading) {
         state.doneLoading = doneLoading;
       },
       SET_ERROR(state, error) {
